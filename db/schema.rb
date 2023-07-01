b1385f67970528f011c14161b4eb99919a4d3df6
Igor Gomes <igor23gomes2@gmail.com>
2023-07-01 18:42:29 -0300

    add schema to order_item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_01_211554) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_01_212232) do
   create_table "clients", force: :cascade do |t|
     t.string "name"
     t.string "document_cpf"
@@ -21,6 +21,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_01_211554) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "product_id", null: false
+    t.float "price"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -66,6 +77,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_01_211554) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
   add_foreign_key "orders", "clients"
   add_foreign_key "orders", "users"
   add_foreign_key "products", "suppliers"
