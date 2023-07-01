fc6f54d660e803c49bf0d3d5568279f71680d546
Igor Gomes <igor23gomes2@gmail.com>
2023-07-01 17:54:56 -0300

    create model product

---
+++ spec/views/products/index.html.erb_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "products/index", type: :view do
+  before(:each) do
+    assign(:products, [
+      Product.create!(
+        name: "Name",
+        description: "Description",
+        amount: 2.5,
+        supplier: nil
+      ),
+      Product.create!(
+        name: "Name",
+        description: "Description",
+        amount: 2.5,
+        supplier: nil
+      )
+    ])
+  end
+
+  it "renders a list of products" do
+    render
+    cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
+    assert_select cell_selector, text: Regexp.new("Name".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new("Description".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(2.5.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(nil.to_s), count: 2
+  end
+end
---
+++ spec/views/products/new.html.erb_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "products/new", type: :view do
+  before(:each) do
+    assign(:product, Product.new(
+      name: "MyString",
+      description: "MyString",
+      amount: 1.5,
+      supplier: nil
+    ))
+  end
+
+  it "renders new product form" do
+    render
+
+    assert_select "form[action=?][method=?]", products_path, "post" do
+
+      assert_select "input[name=?]", "product[name]"
+
+      assert_select "input[name=?]", "product[description]"
+
+      assert_select "input[name=?]", "product[amount]"
+
+      assert_select "input[name=?]", "product[supplier_id]"
+    end
+  end
+end
