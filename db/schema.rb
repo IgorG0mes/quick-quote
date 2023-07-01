638a9c032f4016acace4f1e7ab674ebc646024ee
Igor Gomes <igor23gomes2@gmail.com>
2023-07-01 18:04:49 -0300

    add schema to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_01_205436) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_01_210155) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -30,6 +30,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_01_205436) do
     t.string "email"
     t.string "website"
     t.string "observation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "document_cpf"
+    t.string "date_birth"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
